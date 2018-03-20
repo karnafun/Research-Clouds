7cff9fe37b343cd87f72ba849435bd1f37926d62
Karnafun <danaidor@gmail.com>
2018-03-21 01:45:15 +0200

    Fixed updating "authors" to articles and dispalying it in local JSON. problem is that it is saved as strings and not users. updated the code to write the string is it cannot find Users[i].Name

--- assets/js/Profile.js
+++ assets/js/Profile.js
@@ -54,7 +54,11 @@ function BuildArticles() {
     $.each(User.Articles, function (index, value) {
         var usernames = "";
         for (var i = 0; i < value.Users.length; i++) {
-            usernames += "<span class='article_user'>" + value.Users[i].Name + "</span>";
+            var _name = value.Users[i].Name;
+            if (value.Users[i].Name==undefined) {
+                _name = value.Users[i];
+            }
+            usernames += "<span class='article_user'>" + _name + "</span>";
             if (i != value.Users.length - 1) {
                 usernames += ", ";
             }
@@ -96,7 +100,7 @@ function BuildClusters() {
         /*
         TODO: Build cluster buttons based on value info            
         */
-        resString += '<span onclick="ClusterClick(' + (value.Id) + ')" class="btn light-russian col-md-3 col-lg-2 col-sm-12 animated fadeInLeft" id="uCluster' + (index + 1) + '">' + value.Name + '</span>'
+        resString += '<span onclick="ClusterClick(' + value.Id + ')" class="btn light-russian col-md-3 col-lg-2 col-sm-12 animated fadeInLeft" id="uCluster' + (index + 1) + '">' + value.Name + '</span>'
     });
     $("#clusters").html(resString);
 }
@@ -156,16 +160,7 @@ function ArticleClick() {
 //***************************************************************************************************//
 //**************************************** Fixed Editing ********************************************//
 //**************************************************************************************************//
-/* Renamed the following:
-file1 - file_image
-btnModaleSave = btn_modalSave
-Modal-articleName = articleModal_title
-Modal-articleLink = articleModal_link
-Modal-articleAuthors = articleModal_authors
-
-All fa-edit spans now have ids: edit_name,edit_image, edit_summery edit_article
-this applies to fa-check -> check_name, etc
-applies also to fa-undo -> undo_name, etc
+/* need to clear  unused functions
 
 */
 $(document).ready(function () {
@@ -177,7 +172,7 @@ $(document).ready(function () {
 
     $(document).on("click", function () {
         $("#editUl").hide();
-    })
+    });
 });
 
 function EditArticle(_id) {
@@ -187,14 +182,18 @@ function EditArticle(_id) {
             $("#articleModal_link").val(value.Link);
             var res = "";
             for (var i = 0; i < value.Users.length; i++) {
-                res += value.Users[i].Name;
+                var _name = value.Users[i].Name;
+                if (_name == undefined  ) {
+                    _name = value.Users[i];
+                }
+                res += _name;
                 if (i != value.Users.length - 1) {
                     res += ", ";
                 }
             }
             $("#articleModal_authors").val(res);
         }
-    })
+    });
     $("#articleModal_articleId").val(_id);
 }
 
@@ -210,23 +209,23 @@ function SaveArticle(e) {
     }
 
     var articleId = $("#articleModal_articleId").val();
+    var exists = false;
     for (var i = 0; i < User.Articles.length; i++) {
-        var exists = false;
         var _article = User.Articles[i];
         if (_article.Id == articleId) {
-           
+
             _article.Title = title;
             _article.Link = link;
             try {
-                _users = _users.split(',')
+                _users = _users.split(',');
                 _article.Users = [];
                 for (var j = 0; j < _users.length; j++) {
                     _article.Users.push(_users[j]);
                 }
             } catch (e) {
                 _article.Users = [];
-                _article.Users.push(_users)
-                console.log("tried to split with , the variable: " + _users)
+                _article.Users.push(_users);
+                console.log("tried to split with , the variable: " + _users);
             }
             exists = true;
         }
@@ -244,7 +243,7 @@ function SaveChanges() {
     EditedUser.RegistrationDate = GetDateObject(EditedUser.RegistrationDate);
     request = {
         userString: JSON.stringify(EditedUser)
-    }
+    };
     UpdateUserAjax(request, function (results) {
         if (results.d > 1) {
             alert("User Updated Successfully");
@@ -290,22 +289,22 @@ function ConfigureClickEvents() {
         if (!EditingMode) {
             ToggleEditingTools(true);
         }
-    })
+    });
 
     $(".fa-check-square").click(function () {
         SaveChanges();
-    })
+    });
 
     $(".fa-times").click(function () {
-        CancelChanges();
-    })
+        // CancelChanges();
+    });
     $(".fa-edit").click(function (e) {
         EditField(e);
-    })
+    });
 
     $("#articleModal_btn_save").click(function (e) {
         SaveArticle(e);
-    })
+    });
 
     $("#infoModal_btn_save").click(function (e) {
         User.FirstName = $("#infoModal_firstName").val();
@@ -318,7 +317,7 @@ function ConfigureClickEvents() {
         }
         UpdatePageFromUser();
         UpdateUserInDatabase();
-    })
+    });
 
     $("#summeryModal_btn_save").click(function (e) {
         User.Summery = $("#summeryModal_summery").val();
@@ -342,287 +341,20 @@ function EditField(e) {
     }
 }
 
-
-function CancelChange(e) {
-    var sender = e.currentTarget.id;
-    if (sender == "undo_image") {
-
-    } else if (sender == "undo_name") {
-
-    } else if (sender == "undo_summery") {
-
-    }
-}
-
 function UpdateUserInDatabase() {
     User.BirthDate = GetDateObject(User.BirthDate);
     User.RegistrationDate = GetDateObject(User.RegistrationDate);
     UpdateUserAjax({ userString: JSON.stringify(User) }, function (results) {
         PopAlert('Success', 'You updated your profile');
-    }, errorCB)
+    }, errorCB);
 }
 
-function PopAlert(type,message) {
+function PopAlert(type, message) {
     var popup = [
         '<div class="alert alert-info">',
         '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>',
-        '<strong>' + type+'!</strong> '+message,
+        '<strong>' + type + '!</strong> ' + message,
         '</div>'
     ];
     $('#userProfileCon').prepend(popup.join(''));
 }
-
-//***************************************************************************************************//
-//***************************** Editing Profile (Ilya Testing) *************************************
-//**************************************************************************************************//
-$(document).ready(function () {
-    //Running before ready 
-
-    return;
-
-
-    var userimg = document.getElementById("uImg");
-    var file = document.getElementById("file1");
-    $(file).hide()
-    ///Catch Modale save button//////
-
-    var Savebtn = document.getElementById("articleModal_btn_save")
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-    var div = document.getElementById('uID');
-    var edit = document.getElementsByClassName('fa-edit');
-    var check = document.getElementsByClassName('fa-check');
-    var undo = document.getElementsByClassName('fa-undo');
-    var summery = document.getElementById("uSummery");
-    var editProfile = document.getElementById("editProfile");
-    var doneEdit = document.getElementsByClassName("fa-check-square");
-    var cancelEdite = document.getElementsByClassName("fa-times");
-    $(doneEdit).hide();
-    $(cancelEdite).hide();
-    $(check).hide();
-    $(undo).hide();
-    $(edit).hide();
-    var stringTemp = div.innerHTML;
-    var summeryTemp = summery.innerHTML;
-
-
-    //Show edit button
-    editProfile.onclick = function (e) {
-        $(edit).show();
-        $(cancelEdite).show()
-    }
-
-
-    cancelEdite[0].onclick = function () {
-
-        $(edit).hide();
-        $(doneEdit).hide();
-        $(cancelEdite).hide();
-        $(check).hide();
-        $(undo).hide();
-
-        if (div.contentEditable == true) {
-            div.contentEditable = false;
-        }
-        else {
-            return null;
-        }
-    }
-
-
-
-
-    ///////// Add to body article ////////////////////////////
-
-    Savebtn.onclick = function (e) {
-        var modaleArtical = document.getElementById("uArticleModale");
-        var articleUl = document.getElementById("articleList");
-        var articletitle = document.getElementById("Modal-articleName");
-        var articleLink = document.getElementById("Modal-articleLink");
-        var articleAuthor = document.getElementById("Modal-articleAuthors");
-        if (articletitle.value == '' || articleLink.value == '' || articleAuthor.value == '') {
-            return null;
-        }
-        else {
-
-            articleUl.innerHTML += "<li class='media my-4' style='border-bottom:2px solid #F8FCF7'> <div class='media-body'><h5 class='mt-0 mb-1'><a href='" + articleLink.value + "'>" + articletitle.value + "</a></h5><small>" + articleAuthor.value + "<cite> PHD</cite></small></div></li>";
-            ArticleDetails = { Id: User.Id, Keywords: [], Link: articleLink.value, Title: articletitle.value, Users: [User] };
-            User.Articles.push(ArticleDetails);
-
-        }
-
-
-    }
-
-
-    /////////////////////////////////////////////////////////////////////Changing User Img///////////////////////////////////////////////////////////
-
-
-    edit[0].onclick = function (e) {
-        $(check[0]).show();
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        $(this).hide();
-        var test = file.click();
-        x = file.value;
-    }
-
-    check[0].onclick = function (e) {
-        $(edit[0]).show();
-        $(this).hide();
-        alert(file.value);
-        //var tmppath = URL.createObjectURL(event.target.file[0]);
-        // userimg.setAttribute("src", String(x));
-    }
-
-
-
-    ////////////////////////////End Changing User Img/////////////////////////////////////
-
-    ////////////////////////////////////////////////////////Name changing////////////////////////////////////////////////////
-    // making changes
-    edit[1].onclick = function (e) {
-        stringTemp = div.innerHTML;
-        $(this).hide();
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        $(check[1]).show();
-        $(undo[1]).show();
-        div.contentEditable = true;
-        div.focus();
-        div.setAttribute("class", "edeting");
-        $(doneEdit).show();
-        $(cancelEdite).show();
-    }
-
-
-    //confirmiing chenges
-    check[1].onclick = function (e) {
-        var string = div.innerHTML;
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        $(check[1]).hide();
-        $(undo[1]).hide();
-        if (string == '') {
-            div.innerHTML = "edit Line";
-            $(edit[1]).show();
-            div.contentEditable = false;
-            div.className = "endEdeting";
-
-
-        }
-        else {
-            string = string.trim();
-            // div.style.backgroundColor = '#ffffff';
-            div.style.border = '';
-            div.contentEditable = false;
-            $(edit[1]).show();
-            div.className = "endEdeting";
-            User.Name = string;
-        }
-
-    }
-
-    //Undoing changes
-    undo[1].onclick = function (e) {
-        $(this).hide();
-        $(check[1]).hide();
-        $(edit[1]).show();
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        div.innerHTML = stringTemp.trim();
-        div.contentEditable = false;
-        div.className = "endEdeting";
-
-
-
-    }
-
-
-
-
-    /////////////////////////////////////////////////////////Canging Summery////////////////////////////////////////////////////
-
-    edit[2].onclick = function (e) {
-        summeryTemp = summery.innerHTML;
-        $(this).hide();
-        $(check[2]).show();
-        $(undo[2]).show();
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        summery.contentEditable = true;
-        summery.focus();
-        summery.setAttribute("class", "edeting");
-    }
-
-
-    //confirmiing chenges
-    check[2].onclick = function (e) {
-        var summeryString = summery.innerHTML;
-        $(check[2]).hide();
-        $(undo[2]).hide();
-        $(doneEdit).show();
-        $(cancelEdite).show();
-        if (summeryString == '') {
-            summery.innerHTML = "Add Summery";
-            $(edit[2]).show();
-            summery.contentEditable = false;
-            summery.className = "Endsummery text-left";
-
-
-        }
-        else {
-            summeryString = summeryString.trim();
-            //  summery.style.backgroundColor = '#ffffff';
-            summery.style.border = '';
-            summery.contentEditable = false;
-            $(edit[2]).show();
-            summery.className = "Endsummery text-left";
-
-        }
-
-    }
-
-    //Undoing changes
-    undo[2].onclick = function (e) {
-        $(this).hide();
-        $(check[2]).hide();
-        $(edit[2]).show();
-        summery.innerHTML = summeryTemp.trim();
-        summery.contentEditable = false;
-        summery.className = "Endsummery text-left";
-
-
-
-    }
-
-
-
-
-
-
-    doneEdit[0].onclick = function () {
-        User.Name = div.innerHTML;
-        if (ArticleDetails = ! null) {
-            User.Articles.push(ArticleDetails);
-        }
-        var jsonString = JSON.stringify(User);
-        var request = {
-            userString: jsonString
-        }
-        UpdateUserAjax(request, UserUpdated, errorCB);
-
-    }
-
-
-    function UserUpdated(results) {
-        alert("OK");
-    }
-    function errorCB(error) {
-        console.log(error.responseText);
-    }
-
-})
