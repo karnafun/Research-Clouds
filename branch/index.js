f130da3925869fcb7adc3116c594d76a2f4061dd
ilya1989 <ilyakolker@gmail.com>
2018-06-09 02:03:33 +0300

    good looking aniamtion

--- branch/index.js
+++ branch/index.js
@@ -19,17 +19,39 @@ function LoadUser() {
     }
 
 }
+$(document).ready(function () {
+    var aniTrue = true;
+    redbool = true;
+    window.onresize = function () {
+        $("#widthNum").text($(window).width());
+        
+
+        if (redbool) {
+            if ($(window).width() < 480 && aniTrue == true) {
+                SetMobileDiv();
+                aniTrue = false
+            }
+            else if ($(window).width() > 480 && aniTrue == false) {
+                BigScreenAnimation();
+                aniTrue = true;
+            }
+        }
 
 
+    }
+})
+    
 
+
+var window_width = $(window).width();
+var window_height = $(window).height();
 var go = true; // to not run init() many times
+mobilebool = true;
 let baseX = 300 , baseY = 300;
 let greensX = baseX, greensY = baseY;
 let reds = [];
 let greens = [];
 let purple = GetDiv('purple hidden');
-var window_width = $(window).width();
-var window_height = $(window).height();
 let startX = baseX;
 let startY = baseY;
 let positions = [];
@@ -39,21 +61,27 @@ function ShowPurple() {
 
     p = document.getElementsByClassName("purple");
     p[0].style.opacity = 1;
-
+    
 }
 function PurpleClick(target) {
     if (go) {
         init();
     }
-    ShowReds();
+    ShowReds($(window).width());
     go = false;
 }
 
 function init() {
-
     var count = 2
     for (var i = 0; i < User.Clusters.length; i++) {
         var div = document.createElement('div');
+        if (window_width > 480) {
+            div.setAttribute('class', 'box russian elem');
+        }
+        else {
+            div.setAttribute('class', 'box russian mobileelem');
+
+        }
         div.setAttribute('class', 'box russian elem');
         div.innerHTML = "<p>" + User.Clusters[i].Name + "</p>";
         div.id = i;
@@ -66,28 +94,17 @@ function init() {
     }
    
 }
-function ShowReds() {  
+function ShowReds(ww) {  
     //red onclicks:
-    for (var t = 0; t < 4; t++) {
-        var _div = document.getElementById(t);
-        _div.onclick = function (e) { RedClick(this); };
-        switch (t) {
-            case 0:
-                $("#" + t).animate({ top: 30 + "%", left: 50 + "%", margin: -50 }, 1000);
-                break;
+    if (ww > 480) {
+        BigScreenAnimation();
 
-            case 1:
-                $("#" + t).animate({ top: 50 + "%", left: 60 + "%", margin: -50 }, 1000);
-                break;
-            case 2:
-                $("#" + t).animate({ top: 50 + "%", left: 40 + "%", margin: -50 }, 1000);
-                break;
-            case 3:
-                $("#" + t).animate({ top: 70 + "%", left: 50 + "%", margin: -50 }, 1000);
-                break;
-
-        }
     }
+    else {
+        SetMobileDiv();
+       
+    }
+    
 
 
 }
@@ -95,6 +112,7 @@ function ShowReds() {
 function GetDiv(classString) {
     var div = document.createElement('div');
     div.setAttribute('class', classString);
+    div.setAttribute('id', "u" + User.Id);
     document.getElementById("bd").appendChild(div);
     GetClickEvent(div);
     return div;
@@ -112,13 +130,13 @@ function GetClickEvent(target) {
     };
 }
 function RedClick(target) {
-   
+    redbool = false;
     $('.purple').hide();
     target.onclick = function (e) { BackToClusters(target); };
     _targets = AllDivsExcept(target);
-    $(_targets).remove();
+    $(_targets).hide();
 
-    $("#"+target.id).animate({ top: 50 + "%", left: 50 + "%", margin: -100, height: 200 + "px", width: 200 + "px" }, 1000);
+    $("#"+target.id).animate({ top: 50 + "%", left: 50 + "%", height: 0, width: 10 + "%",'padding-bottom': 10+"%" }, 1000);
     for (var i = 0; i < _targets.length; i++) {
         _targets[i].onclick = function () { return false; };
     }
@@ -131,11 +149,22 @@ function GreenClick(target) {
 }
 
 function ShowGreens() {
-    var h = ((window_height / 4) / Math.floor(Math.random() * 11 + 9)) ;
-    var w = ((window_width / 5) / Math.floor(Math.random() * 11 + 9)) ;
-    for (var i = 0; i < greens.length; i++) {
-        $("#" + greens[i].id).animate({ top: h + (i * Math.random() * 10) + "%", left: w + (i * 10) + "%", margin: -50, height: 100, width: 100 }, 1000);
+
+    var h = (Math.floor(Math.random() *1) + 5)* 10;
+    var w = (Math.floor(Math.random() *2) + 2) * 10;
+    countP = 0;
+    countPX = 0;
+    if (greens.length % 2 == 0) {
+        var ww = $(window).width();
+        var up = greens.length / 2;
+        AnimateGreens(up, h, w,ww)
     }
+    else {
+        var up = ((greens.length + 1) / 2);
+        var ww = $(window).width();
+        AnimateGreens(up,h,w,ww)
+    }
+
 }
 
 function AllDivsExcept(target) {
@@ -152,65 +181,104 @@ function AllDivsExcept(target) {
 
 
 
-function ConfigurePositions() {
-    startX = baseX;
-    startY = baseY;
-    positions_upperLeft = [
-        { x: startX - 150, y: startY, open: true },
-        { x: startX - 300, y: startY, open: true },
-        { x: startX - 450, y: startY, open: true },
-        { x: startX - 600, y: startY, open: true },
-        { x: startX, y: startY - 140, open: true },
-        { x: startX - 150, y: startY - 140, open: true },
-        { x: startX - 300, y: startY - 130, open: true },
-        { x: startX - 450, y: startY - 150, open: true },
-        { x: startX - 600, y: startY - 170, open: true }
-    ];
+//function ConfigurePositions() {
+//    startX = baseX;
+//    startY = baseY;
+//    positions_upperLeft = [
+//        { x: startX - 150, y: startY, open: true },
+//        { x: startX - 300, y: startY, open: true },
+//        { x: startX - 450, y: startY, open: true },
+//        { x: startX - 600, y: startY, open: true },
+//        { x: startX, y: startY - 140, open: true },
+//        { x: startX - 150, y: startY - 140, open: true },
+//        { x: startX - 300, y: startY - 130, open: true },
+//        { x: startX - 450, y: startY - 150, open: true },
+//        { x: startX - 600, y: startY - 170, open: true }
+//    ];
 
-    positions_upperRight = [
-        { x: startX + 150, y: startY, open: true },
-        { x: startX + 300, y: startY, open: true },
-        { x: startX + 450, y: startY, open: true },
-        { x: startX + 600, y: startY, open: true },
-        { x: startX, y: startY + 140, open: true },
-        { x: startX + 150, y: startY - 140, open: true },
-        { x: startX + 300, y: startY - 130, open: true },
-        { x: startX + 450, y: startY - 150, open: true },
-        { x: startX + 600, y: startY - 170, open: true }
-    ];
+//    positions_upperRight = [
+//        { x: startX + 150, y: startY, open: true },
+//        { x: startX + 300, y: startY, open: true },
+//        { x: startX + 450, y: startY, open: true },
+//        { x: startX + 600, y: startY, open: true },
+//        { x: startX, y: startY + 140, open: true },
+//        { x: startX + 150, y: startY - 140, open: true },
+//        { x: startX + 300, y: startY - 130, open: true },
+//        { x: startX + 450, y: startY - 150, open: true },
+//        { x: startX + 600, y: startY - 170, open: true }
+//    ];
 
-    positions_lowerRight = [
-        { x: startX, y: startY + 140, open: true },
-        { x: startX + 150, y: startY + 140, open: true },
-        { x: startX + 300, y: startY + 130, open: true },
-        { x: startX + 450, y: startY + 150, open: true },
-        { x: startX + 600, y: startY + 170, open: true }
-    ];
+//    positions_lowerRight = [
+//        { x: startX, y: startY + 140, open: true },
+//        { x: startX + 150, y: startY + 140, open: true },
+//        { x: startX + 300, y: startY + 130, open: true },
+//        { x: startX + 450, y: startY + 150, open: true },
+//        { x: startX + 600, y: startY + 170, open: true }
+//    ];
 
-    positions_lowerLeft = [
-        { x: startX - 150, y: startY + 140, open: true },
-        { x: startX - 300, y: startY + 130, open: true },
-        { x: startX - 450, y: startY + 150, open: true },
-        { x: startX - 600, y: startY + 170, open: true }
-    ];
+//    positions_lowerLeft = [
+//        { x: startX - 150, y: startY + 140, open: true },
+//        { x: startX - 300, y: startY + 130, open: true },
+//        { x: startX - 450, y: startY + 150, open: true },
+//        { x: startX - 600, y: startY + 170, open: true }
+//    ];
 
+//}
+
+function AnimateGreens(up, h, w,ww) {
+    if (ww > 480) {
+        for (var i = 0; i < greens.length; i++) {
+            wTest = (w + countPX);
+            hTest = (h + countP);
+            if (hTest == 50 && wTest == 50) {
+                
+                $("#" + greens[i].id).animate({ top: (h + 12) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+            }
+            else {
+                if (up <= 0) {
+                    countP += 10;
+                    $("#" + greens[i].id).animate({ top: (h + countP) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+                    
+                }
+                else {
+                    $("#" + greens[i].id).animate({ top: (h + countP) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+                    countP -=10;
+                }
+            }
+
+            countPX += 10;
+            up--;
+        }
+    }
+    else {
+        for (var i = 0; i < greens.length; i++) {
+            wTest = (w + countPX);
+            hTest = (h + countP);
+            if (hTest == 50 || wTest == 50) {
+                countP += 10;
+                $("#" + greens[i].id).animate({ top: (h + countP) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+            }
+            else {
+                if (up <= 0) {
+                    countP += 10;
+                    $("#" + greens[i].id).animate({ top: (h + countP) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+
+                }
+                else {
+                    $("#" + greens[i].id).animate({ top: (h + countP) + "%", left: (w + countPX) + "%", height: 0, width: 6 + "%", 'padding-bottom': 6 + "%" }, 1000);
+                    countP -= 10;
+                }
+            }
+
+            countPX += 10;
+            up--;
+        }
+    }
+        
+    
+    
 }
 
-function AnimateGreens() {
-
-    var counter = 0;
-
-        anime({
-            targets: greens,
-            opacity: 1,
-            translateX: [{ value: function() {return anime.random(-200, 250) }, transition: 800 }, { value: 0, transition: 600 }],
-            translateY: [{ value: function () { return anime.random(-200, 250) }, transition: 800 }, { value: 0, transition: 600 }],
-            easeeasing: 'easeInOutSine',
-           delay: function(el, i, l) { return i * 1000; },
-
-    });
-
-}
 
 function GenerateClusters(_id) {
     var clust = User.Clusters[_id];
@@ -218,9 +286,10 @@ function GenerateClusters(_id) {
     var counter = 1;
     for (var i = 0; i < clust.Users.length; i++) {
         var div = document.createElement('div');
-        div.className = 'box green elem';
+        div.className = 'green elem';
         div.style.backgroundImage = "url('" + clust.Users[i].ImagePath + "')";
         div.style.backgroundSize = 'cover';
+        div.style.backgroundRepeat = "no-repeat";
         div.style.zIndex = "-2";
         div.id = 'r' + clust.Users[i].Id;
         greens[greens.length] = div;
@@ -251,4 +320,61 @@ function BackToClusters(target, targets) {
     $('.purple').show();
     PurpleClick();
     ShowPurple();
+    redbool = true;
+}
+
+/////////////////////////////////////Mobile functions/////////////////////////////////////////////////////
+function BigScreenAnimation() {
+    for (var t = 0; t < 4; t++) {
+        var _div = document.getElementById(t);
+        if (mobilebool == false) {
+            $("#u" + User.Id).show();
+        }
+        _div.onclick = function (e) { RedClick(this); };
+        switch (t) {
+            case 0:
+                $("#" + t).animate({ top: 32 + "%", left: 50 + "%" ,width: 6 + "%", height: 0, 'padding-bottom': 6 + "%" }, 1000);
+                break;
+
+            case 1:
+                $("#" + t).animate({ top: 50 + "%", left: 60 + "%", width: 6 + "%", height: 0, 'padding-bottom': 6 + "%" }, 1000);
+                break;
+            case 2:
+                $("#" + t).animate({ top: 68 + "%", left: 50 + "%", width: 6 + "%", height: 0, 'padding-bottom': 6 + "%" }, 1000);
+                break;
+            case 3:
+                $("#" + t).animate({ top: 50 + "%", left: 40 + "%", width: 6 + "%", height: 0, 'padding-bottom': 6 + "%" }, 1000);
+                break;
+
+        }
+    }
+    mobilebool = true;
+}
+
+function SetMobileDiv() {
+    for (var t = 0; t < 4; t++) {
+        var _div = document.getElementById(t);
+        if (mobilebool == true) {
+            $("#u" + User.Id).hide();
+        }
+        
+        _div.onclick = function (e) { RedClick(this); };
+        switch (t) {
+            case 0:
+                $("#" + t).animate({ top: 35 + "%", left: 35 + "%", width: 10 + "%", height: 0, 'padding-bottom': 10 + "%" }, 1000);
+                break;
+
+            case 1:
+                $("#" + t).animate({ top: 35 + "%", left: 65 + "%", width: 10 + "%", height: 0, 'padding-bottom': 10 + "%" }, 1000);
+                break;
+            case 2:
+                $("#" + t).animate({ top: 65 + "%", left: 65 + "%", width: 10 + "%", height: 0, 'padding-bottom': 10 + "%" }, 1000);
+                break;
+            case 3:
+                $("#" + t).animate({ top: 65 + "%", left: 35 + "%", width: 10 + "%", height:0, 'padding-bottom': 10+"%" }, 1000);
+                break;
+
+        }
+    }
+    mobilebool = false;
 }
