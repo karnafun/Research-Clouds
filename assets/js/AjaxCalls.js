88ed418d6310e870287fd70bfbe7779020b31d24
ilya1989 <ilyakolker@gmail.com>
2018-07-23 22:11:34 +0300

    edit profile modal

--- assets/js/AjaxCalls.js
+++ assets/js/AjaxCalls.js
@@ -134,3 +134,19 @@ function InsertUserAjax(request, successCB, failureCB) {
         error: failureCB
     })// end of ajax call
 }
+
+
+function FindUserAutomatically(request, successCB, failureCB) {
+
+    dataString = JSON.stringify(request);
+    $.ajax({ // ajax call starts
+        url: '../../AjaxServices.asmx/FindUserAutomatically',   // JQuery call to the server side method
+        data: dataString,    // the parameters sent to the server
+        type: 'POST',        // can be post or get
+        dataType: 'json',    // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8', // of the data received
+        timeout: 5000, // timeout in miliseconds
+        success: successCB, // 
+        error: failureCB
+    })// end of ajax call
+}
